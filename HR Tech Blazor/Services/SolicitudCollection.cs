7e7e3ac7a50e75483eef76614e5fec020bbca36f
Erik-Humberto <134341785+Erik-Humberto@users.noreply.github.com>
2024-03-01 19:08:00 -0600

    nuevos cambios
    se agrego nuevos cambios

---
+++ HR Tech Blazor/Services/SolicitudCollection.cs
@@ -0,0 +1,12 @@
+﻿using HR_Tech_Blazor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Tech_Blazor.Services {
+    public class SolicitudCollection {
+        public Dictionary<string, Solicitud> Solicitudes { get; set; }
+    }
+}
