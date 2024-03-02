0530d55d4d624d9832ac9bde7e7b4db323ecc0a8
Erik-Humberto <134341785+Erik-Humberto@users.noreply.github.com>
2024-03-01 19:05:40 -0600

    Nuevos cambios
    nuevo module

---
+++ HR Tech Blazor/Models/Solicitud.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Tech_Blazor.Models {
+    public   class Solicitud {
+        public int IdSolicitud { get; set; }
+        public string Motivo { get; set; }
+        public string Nombre { get; set; }
+        public int DiasSolicitados { get; set; }
+        public bool Status { get; set; }
+    }
+}
